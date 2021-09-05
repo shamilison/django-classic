b5e37781beaa8efdd3ddf8262bad7bacb772a265
Shakibul Islam <shakib@fintechlab.dev>
2021-09-06 01:46:27 +0600

    #5 - Designed API for every model data types

--- api_views/classic_api_view.py
+++ api_views/classic_api_view.py
@@ -10,11 +10,9 @@ from django.apps import apps
 from django.contrib.auth import get_user_model
 from django.db import transaction
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import status, mixins, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.exceptions import APIException
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.renderers import BrowsableAPIRenderer
 from rest_framework.response import Response
@@ -26,7 +24,7 @@ from django_classic.serializers.classic_serializers import ClassicTokenSerialize
 log = logging.getLogger(__name__)
 
 
-class ClassicAPIViewSet(viewsets.ModelViewSet):
+class ClassicAPIGetViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     authentication_classes = (ClassicTokenAuthentication, ClassicSessionAuthentication,)
     # permission_classes = (IsAuthorized,)
     renderer_classes = (ClassicJSONRenderer, BrowsableAPIRenderer)
@@ -34,16 +32,16 @@ class ClassicAPIViewSet(viewsets.ModelViewSet):
 
     @csrf_exempt
     def dispatch(self, request, *args, **kwargs):
-        return super(ClassicAPIViewSet, self).dispatch(request, *args, **kwargs)
+        return super(ClassicAPIGetViewSet, self).dispatch(request, *args, **kwargs)
 
     def finalize_response(self, request, response, *args, **kwargs):
-        return super(ClassicAPIViewSet, self).finalize_response(request, response, *args, **kwargs)
+        return super(ClassicAPIGetViewSet, self).finalize_response(request, response, *args, **kwargs)
 
     def options(self, request, *args, **kwargs):
-        return super(ClassicAPIViewSet, self).options(request, *args, **kwargs)
+        return super(ClassicAPIGetViewSet, self).options(request, *args, **kwargs)
 
     def list(self, request, *args, **kwargs):
-        response = super(ClassicAPIViewSet, self).list(request, *args, **kwargs)
+        response = super(ClassicAPIGetViewSet, self).list(request, *args, **kwargs)
         return response
 
     def get_queryset(self, **kwargs):
@@ -51,11 +49,11 @@ class ClassicAPIViewSet(viewsets.ModelViewSet):
         return queryset
 
 
-class ClassicVersionAPIViewSet(ClassicAPIViewSet):
+class ClassicVersionAPIViewSet(ClassicAPIGetViewSet):
     version_pattern = re.compile("^\d+$")
 
     def model_serializer_finder(self, model_class, version, *args, **kwargs):
-        version_path = "v{}".format(str(version))
+        version_path = "v{}".format(version)
         app_path = None
         try:
             # getting the app name
@@ -76,7 +74,7 @@ class ClassicVersionAPIViewSet(ClassicAPIViewSet):
             # from model object getting the path og the model file
             model_path = os.path.abspath(sys.modules[model_object.__module__].__file__)
             # extracting the model file name from the path
-            model_file_name = model_path[model_path.rfind("/") + 1:model_path.rfind(".")]
+            model_file_name = model_path[model_path.rfind(os.sep) + 1:model_path.rfind(".")]
         except Exception as error:
             log.exception(error)
         # difference in path
@@ -85,43 +83,37 @@ class ClassicVersionAPIViewSet(ClassicAPIViewSet):
         # difference between the app_model_path and model_path
         difference = os.path.relpath(model_path, app_model_path)
         # finding the last slash index
-        last_slash_index = difference.rfind("/")
+        last_slash_index = difference.rfind(os.sep)
         # if there is a slash this means the model_file is deep inside the models directory
         if last_slash_index != -1:
             # excluding file name, getting the different path only
             difference = difference[:last_slash_index]
-            # if the path is too deep we need to replace ``/`` with ``.``
-            # difference = difference.replace("/", ".")
+            # if the path is too deep we need to replace `os.sep` with ``.``
+            # difference = difference.replace(os.sep, ".")
             # adding a trailing ``dot``
-            difference += "/"
+            difference += os.sep
         # There is no slash so the model file is just inside the app_model_path directory
         else:
             difference = ""
-        try:
-            # Creating the exact file path
-            file_path = "{}/serializers/{}/{}".format(
-                app_path, version_path, difference)
-            file_name = '{}_serializer.py'.format(model_file_name)
-            # getting the exact serializer module
-            module_spec = importlib.util.spec_from_file_location(
-                model_name + "Serializer", os.path.join(file_path, file_name))
-            serializer_module = importlib.util.module_from_spec(module_spec)
-            module_spec.loader.exec_module(serializer_module)
-            return getattr(serializer_module, model_name + "Serializer")
-        except Exception as error:
-            raise APIException(error)
+        # Creating the exact file path
+        file_path = os.path.join(app_path, 'serializers', version_path, difference)
+        file_name = '{}_serializer'.format(model_file_name)
+        # getting the exact serializer module
+        module_spec = importlib.util.spec_from_file_location(
+            file_name, os.path.join(file_path, '{}.py'.format(file_name)))
+        serializer_module = importlib.util.module_from_spec(module_spec)
+        module_spec.loader.exec_module(serializer_module)
+        serializer_class = getattr(serializer_module, model_name + "Serializer")
+        setattr(serializer_class.Meta, 'ref_name', '{}_{}'.format(file_name, self.action))
+        return serializer_class
 
     def get_serializer_class(self):
         # getting the api version name
-        version = None
         model_class = self.model
         if self.request.version and self.version_pattern.match(self.request.version):
-            try:
-                version = int(self.request.version)
-            except Exception as error:
-                log.exception(error)
+            version = int(self.request.version)
         else:
-            return model_class.get_serializer()
+            version = 1
         model_serializer = self.model_serializer_finder(model_class=model_class, version=version)
         if model_serializer:
             return model_serializer
