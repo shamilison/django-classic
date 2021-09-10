bbb4249f3c952c299cf5ac1c298ded4bae364662
Shakibul Islam <shakib@fintechlab.dev>
2021-09-10 19:32:02 +0600

    #1
    - Fixed the sorting issue

--- controllers/classic_pagination.py
+++ controllers/classic_pagination.py
@@ -143,7 +143,7 @@ class ClassicCursorAPIPagination(CursorPagination):
         except (KeyError, ValueError):
             return 0
 
-    def get_sorting(self, request):
+    def get_sorting(self, queryset, request):
         try:
             return request.query_params[self.sort_query_param],
         except (KeyError, ValueError):
