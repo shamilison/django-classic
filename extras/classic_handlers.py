9dfec680fc05f46a0e73447279fcf4a2483f14c5
Shakibul Islam <shakib@fintechlab.dev>
2021-09-12 15:31:55 +0600

    #1
    - Exception logger in API calls

--- extras/classic_handlers.py
+++ extras/classic_handlers.py
@@ -2,8 +2,15 @@ from rest_framework.views import exception_handler
 
 __author__ = 'shamilsakib'
 
+import logging
+from django_classic.logging.classic_logger import ClassicLogger
+
+logging.setLoggerClass(ClassicLogger)
+logger = logging.getLogger(__name__)
+
 
 def classic_api_error_handler(exc, *args):
+    logger.exception(exc)
     response = exception_handler(exc, *args)
     if response is not None:
         response.data['status_code'] = response.status_code
