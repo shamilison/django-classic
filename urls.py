69e566bffe529f370b22c2ca5c5173b9414e5f0b
Shakibul Islam <shakib@fintechlab.dev>
2021-10-06 22:41:26 +0600

    - Comment level modification

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-"""market_data_reader URL Configuration
+"""django_classic URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
