92d627b111c576f7c98e96fff50ea676f6c62001
Shakibul Islam <shakib@fintechlab.dev>
2021-12-30 19:35:33 +0600

    #197
    -- Instrument ID passing issue fixed

--- logging/classic_logger.py
+++ logging/classic_logger.py
@@ -5,11 +5,31 @@ from django.conf import settings
 
 class ClassicLogger(logging.getLoggerClass()):
 
+    def __init__(self, name):
+        """
+        Initialize the logger with a name and an optional level.
+        """
+        super(ClassicLogger, self).__init__(name=name, level="INFO")
+
+    def info(self, msg, *args, **kwargs):
+        """
+        Log 'msg % args' with severity 'INFO'.
+
+        To pass exception information, use the keyword argument exc_info with
+        a true value, e.g.
+
+        logger.info("Houston, we have a %s", "interesting problem", exc_info=1)
+        """
+        if not settings.CONSOLE_PRINT:
+            super(ClassicLogger, self).info(msg, *args, **kwargs)
+        else:
+            print(msg)
+
     def exception(self, msg, *args, exc_info=True, **kwargs):
         """
         Convenience method for logging an ERROR with exception information.
         """
-        if not settings.DEBUG:
+        if not settings.CONSOLE_PRINT:
             super(ClassicLogger, self).exception(msg, *args, exc_info=True, **kwargs)
         else:
             raise Exception(msg)
