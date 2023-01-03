c1d172a2985a13865b5d3572e8f2fa92da2ee83f
Shakibul Islam <shakib@fintechlab.dev>
2023-01-03 22:11:26 +0100

    -- POST a list of objects

--- api_views/classic_api_view.py
+++ api_views/classic_api_view.py
@@ -139,7 +139,12 @@ class ClassicVersionAPICreateViewSet(ClassicVersionAPIViewSet, mixins.CreateMode
 
     def create(self, request, *args, **kwargs):
         try:
-            return super(ClassicVersionAPICreateViewSet, self).create(request, *args, **kwargs)
+            serializer = self.get_serializer(
+                data=request.data, many=isinstance(request.data, list))
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
         except Exception as error:
             raise APIException(error)
 
