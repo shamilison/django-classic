07ad09a1ca9fdfcd19c85b69dd0f873e184f39e5
Shakibul Islam <shakibul.islam@nkg.coffee>
2023-07-17 14:50:37 +0200

    #34
    - Login issue fixed

--- extras/classic_handlers.py
+++ extras/classic_handlers.py
@@ -1,3 +1,6 @@
+from django.http import JsonResponse
+from jwt import exceptions
+from rest_framework.status import HTTP_403_FORBIDDEN
 from rest_framework.views import exception_handler
 
 __author__ = 'shamilsakib'
@@ -14,4 +17,15 @@ def classic_api_error_handler(exc, *args):
     response = exception_handler(exc, *args)
     if response is not None:
         response.data['status_code'] = response.status_code
+    else:
+        _auth_error = isinstance(exc, exceptions.InvalidSignatureError) \
+                      or isinstance(exc, exceptions.ExpiredSignatureError) \
+                      or isinstance(exc, exceptions.InvalidTokenError) \
+                      or isinstance(exc, exceptions.ExpiredSignatureError) \
+                      or isinstance(exc, exceptions.ExpiredSignatureError)
+        if _auth_error:
+            response = JsonResponse(data={
+                "message": str(exc),
+                "success": False,
+            }, status=HTTP_403_FORBIDDEN)
     return response
